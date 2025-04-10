ad7ea18b5a4d486d1daba45fd6fa667cda5a9e28
gabrielhbrioto <gabriel.h.brioto@gmail.com>
2025-04-09 22:13:35 -0300

    Parser files created

---
+++ src/parser.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "../headers/errorMessages.h"
+#include "../headers/automaton.h"
+#include "../headers/parser.h"
+//#include "lista.h"
+
+
+void get_token(char **token, char *class, int *command) {
+
+}
